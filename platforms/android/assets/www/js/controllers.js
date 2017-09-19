431da70bcd193d22799dc494463f45aef05d4f94
Saqib Khan <najam1992@gmail.com>
2017-09-20 01:04:17 +0500

    many updates

--- platforms/android/assets/www/js/controllers.js
+++ platforms/android/assets/www/js/controllers.js
@@ -25,24 +25,13 @@ function ($scope, $stateParams,$ionicLoading, $ionicPopup, myService, $location,
         $timeout(function() { $state.go($state.current, {}, {reload: true});}, 3);
       };
 
-    $scope.bookMe = function(id){
+    $scope.bookMe = function(name, id){
         var grdObj = {status: "yes", id: id};
-        myService.ground(grdObj).success(function(res){
-            console.log(grdObj, "Checking ground status")
-            console.log(res);
-            if (res === true) {
-                var alertPopup = $ionicPopup.alert({
-                title: 'Booking Confirm',
-                template: 'Thanks For Booking'
-                });
-        }
-        else{
-            var alertPopup = $ionicPopup.alert({
-                title: 'Error',
-                template: 'Groung Not available'
-                });
-        }
-        })
+        var name = {name: name};
+        localStorage.setItem('grdObj', JSON.stringify(grdObj));
+        localStorage.setItem('name', JSON.stringify(name));
+        console.log(grdObj);
+       $state.go('menu.finalOrder');
         
     };
 }])
@@ -57,10 +46,15 @@ function ($scope, $stateParams, $ionicLoading, $ionicPopup, myService, $location
     }
     //signup function
     $scope.submit = function(name, email, password){
+        $ionicLoading.show({
+      template: '<p>Loading...</p><ion-spinner icon="lines"></ion-spinner>'
+     
+    });
         userObj = {name:name, email: email, password: password};
         console.log(userObj,"signup ka function")
         localStorage.setItem('userObj', JSON.stringify(userObj));
         myService.registerUser(userObj).success(function (res) {
+            $ionicLoading.hide();
             console.log(res);
             if(res == true){
                 $location.path('side-menu21/login')
@@ -86,6 +80,10 @@ function ($scope, $stateParams, $ionicLoading, $ionicPopup, myService, $location
 function ($scope, $stateParams, $ionicLoading, $ionicPopup, myService, $location, $timeout) {
 //login function
 	$scope.submit = function (email, password){
+        $ionicLoading.show({
+      template: '<p>Loading...</p><ion-spinner></ion-spinner>'
+     
+    });
 
 		userObj = {email: email, password: password};
 
@@ -95,6 +93,7 @@ function ($scope, $stateParams, $ionicLoading, $ionicPopup, myService, $location
 
 		myService.loginUser(userObj).success(function (res) {
             console.log(res)
+            $ionicLoading.hide();
             if(res.success == true){
                 localStorage.setItem('token', res.token);
                 console.log(localStorage.getItem('token'));
@@ -138,10 +137,38 @@ function ($scope, $stateParams) {
 
 }])
    
-.controller('finalOrderCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('finalOrderCtrl', ['$scope', '$stateParams','$ionicLoading','$ionicPopup','myService','$location','$timeout', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
+function ($scope, $stateParams, $ionicLoading, $ionicPopup, myService, $location, $timeout) {
+    
+    var groundObj = JSON.parse(localStorage.getItem('grdObj'));
+    $scope.name = JSON.parse(localStorage.getItem('name'));
+    console.log(groundObj , name);
+    $scope.submit= function(){
+        $ionicLoading.show({
+      template: '<p>Loading...</p><ion-spinner></ion-spinner>'
+     
+    });
+
+         myService.ground(groundObj).success(function(res){
+            console.log(groundObj, "Checking ground status")
+            console.log(res);
+            $ionicLoading.hide();
+            if (res === true) {
+                var alertPopup = $ionicPopup.alert({
+                title: 'Booking Confirm',
+                template: 'Thanks For Booking'
+                });
+        }
+        else{
+            var alertPopup = $ionicPopup.alert({
+                title: 'Error',
+                template: 'Groung Not available'
+                });
+        }
+        })
+    }
 
 
 }])
